e16455cfd735dba05176a5148d7ab95beb97c780
emiroow <amir_esmaeelzadeh@yahoo.com>
2024-06-03 15:17:45 +0330

    fix: push have bug

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,37 @@
 import express from "express";
-const app = express();
-const port = 3000;
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-app.get("/", (req, res) => {
-  res.send("Hello World !");
+const app = express();
+const uri =
+  "mongodb://root:vyF5ciYhcyGwT7sWs6tcnc1f@emiroowdb:27017/my-app?authSource=admin&replicaSet=rs0&directConnection=true";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.route("/test").get((req: any, res: any) => {
+  res.json({ test: "test" });
 });
 
-console.log("gooooooooooooooooooooooo");
-
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
