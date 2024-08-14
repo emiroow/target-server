085d187f8e7917cef47fcf77e36996a285a5fe99
emiroow <amir_esmaeelzadeh@yahoo.com>
2024-08-15 00:10:47 +0330

    fix: push

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,6 +1,8 @@
 import { Response } from "express";
+import { Error } from "mongoose";
 import { userModel } from "../../src/models/user";
 import { request } from "../../src/types/request";
+import { responseHandler } from "../utils/common/responseHandler";
 
 export const getUsersListController = async (req: request, res: Response) => {
   try {
@@ -9,4 +11,16 @@ export const getUsersListController = async (req: request, res: Response) => {
   } catch (error) {
     return res.status(500).json({ error: error, status: false });
   }
+};
+
+export const getUserInfoController = async (req: request, res: Response) => {
+  const user = await userModel.findById(req.user._id).select(["-password"]);
+  if (!user) throw new Error("user not found !");
+  return responseHandler({
+    res,
+    massage: "success",
+    status: true,
+    data: user,
+    responseCode: 200,
+  });
 };
--- src/routes/user.api.ts
+++ src/routes/user.api.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { checkUserAuthentication } from "../../src/middlewares/auth.middleware";
-import { getUsersListController } from "../controllers/user.controllers";
+import {
+  getUserInfoController,
+  getUsersListController,
+} from "../controllers/user.controllers";
 require("express-async-errors");
 
 const router: Router = Router();
 
 router.get("/list", checkUserAuthentication, getUsersListController);
+
+router.get("/info", checkUserAuthentication, getUserInfoController);
 
 export const usersRouter = router;
