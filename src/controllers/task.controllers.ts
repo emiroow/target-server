75af80ee4169dafe009fe193f19d721bfb44c76c
emiroow <amir_esmaeelzadeh@yahoo.com>
2024-11-29 15:35:09 +0330

    push 😑

--- src/controllers/task.controllers.ts
+++ src/controllers/task.controllers.ts
@@ -1,4 +1,6 @@
 import { Response } from "express";
-import { request } from "types/request";
+import { request } from "../types/request";
 
 export const getTasksController = (req: request, res: Response) => {};
+export const updateTasksController = (req: request, res: Response) => {};
+export const deleteTasksController = (req: request, res: Response) => {};
--- src/routes/task.api.ts
+++ src/routes/task.api.ts
@@ -1,8 +1,15 @@
-import { getTasksController } from "controllers/task.controllers";
 import { Router } from "express";
+import {
+  deleteTasksController,
+  getTasksController,
+  updateTasksController,
+} from "../controllers/task.controllers";
+import { checkUserAuthentication } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-router.get("/list", getTasksController);
+router.get("/list", checkUserAuthentication, getTasksController);
+router.put("/update/:id", checkUserAuthentication, updateTasksController);
+router.delete("/delete/:id", checkUserAuthentication, deleteTasksController);
 
 export const taskRouter = router;
